a23c8f7afa1e5079a9cbf46c4e71adba6999ba15
vortex314 <lieven.merckx@gmail.com>
2020-12-29 19:39:54 +0100

    pthread at end of linking

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/lieven/workspace/wiringMqtt
+# Build directory: /home/lieven/workspace/wiringMqtt/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
